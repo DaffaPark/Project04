fc8554a77095e4e030559d31d7b7c181b76df797
DaffaPark_ <dfa123gm@gmail.com>
2024-06-04 14:05:29 +0800

    add: hello.cpp and sampel code hello world

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+
+#include <iostream>
+
+int main(){
+
+  cout << "=========== PROGRAM OUTPUT ===========" << endl;
+  cout << "Daffa Maulana Ramadhan\n";
+  cout << "C030323001\n";
+  cout << "TI-2B\n";
+  cout << "Saya Suka Beladiri, Bermain Alat Musik, Dan Coding\n";
+  cout << "======================================" << endl;
+
+  return 0;
+}
